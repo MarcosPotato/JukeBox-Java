d7c07ef3bb07548e7587668159e0809d539eb42d
MarcosPotato <marquinhos2725@hotmail.com>
2020-10-19 20:52:52 -0300

    End Database Operations

--- src/JukeBox.java
+++ src/JukeBox.java
@@ -1,24 +1,60 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import repository.MusicsRepository;
+import java.util.Scanner;
+
+import models.Musics;
 
 public class JukeBox {
     public static void main(String[] args) {
         MusicsRepository MusicControl = new MusicsRepository();
 
-        MusicControl.insertNewMusic(1, "Name 1", "Artist1", "2:45", "Rock");
+        Scanner scanf = new Scanner(System.in);
+        int option = 5;
 
-        try{
-            ResultSet rows = MusicControl.listAllMusic();
+        do{
+            System.out.println("Welcome to jukebox");
+            System.out.println("Select a option");
+            System.out.println("\n");
+            System.out.println("1- Insert Music");
+            System.out.println("2- List All Musics");
+            System.out.println("3- Delete Music");
+            System.out.println("\n");
+            System.out.println("0- Exit Jukebox");
+            System.out.println("\n");
 
-            while (rows.next()) {
-                System.out.println(rows.getInt("id") +  "\t" + 
-                                   rows.getString("name") + "\t" +
-                                   rows.getDouble("capacity"));
+            option = scanf.nextInt();
+            
+            switch(option){
+                case 0:
+                    break;
+                case 1:
+                    Musics music = new Musics();
+
+                    System.out.println("Enter music name: ");
+                    scanf.nextLine();
+                    music.setName(scanf.nextLine());
+                    System.out.println("Enter music artist: ");
+                    music.setArtist(scanf.nextLine());
+                    System.out.println("Enter music duration: ");
+                    music.setDuration(scanf.nextLine());
+                    System.out.println("Enter music category: ");
+                    music.setCategory(scanf.nextLine());
+
+                    MusicControl.insertNewMusic(music);
+                    break;
+                case 2:
+                    MusicControl.listAllMusic();
+                    break;
+                case 3:
+                    MusicControl.listAllMusic();
+                    System.out.println("Select music id has be delete: ");
+                    MusicControl.deleteMusic(scanf.nextInt());
+                    break;
+                default:
+                    System.out.println("Invalid Option");
+                    break;
             }
-        } catch(SQLException err){
-            System.err.println(err.getMessage());
-        }
+        }while(option != 0);
+        
+        scanf.close();
     }
 }
--- src/repository/MusicsRepository.java
+++ src/repository/MusicsRepository.java
@@ -6,20 +6,35 @@ import database.ConnectDataBase;
 import models.Musics;
 
 public class MusicsRepository {
+
     Musics music = new Musics();
     ConnectDataBase connect = new ConnectDataBase();
 
 
-    public boolean insertNewMusic(int id, String name, String artist, String duration, String category){
-        music.setId(id);
-        music.setName(name);
-        music.setArtist(artist);
-        music.setDuration(duration);
-        music.setCategory(category);
+    public boolean insertNewMusic(Musics music){
 
         Connection conn = connect.connectDB();
+
+        String countSql = "SELECT COUNT(*) FROM musics";
+        String sql = "INSERT INTO musics(id, name, artist, duration, category) VALUES(?,?,?,?,?)";
+
+        ResultSet musicsCount = null;
+        int count = 0;
+
+        try{
+            Statement stmt  = conn.createStatement();
+            musicsCount = stmt.executeQuery(countSql);
+
+            while(musicsCount.next()){
+                count = musicsCount.getInt(1);
+            }
+            
+            music.setId(count++);
+        } catch(SQLException err){
+            System.err.println(err.getMessage());
+        }
+
         
-        String sql = "INSERT INTO musics(id, name, artist, duration, category) VALUES(?,?,?,?,?,?)";
 
         try (PreparedStatement pstmt = conn.prepareStatement(sql)){
             pstmt.setInt(1, music.getId());
@@ -38,12 +53,23 @@ public class MusicsRepository {
     }
 
     public void deleteMusic(int id){
-        /**
-         * Deletar musica no banco
-         */
+        Connection conn = connect.connectDB();
+
+        String sql = "DELETE FROM musics WHERE id = ?";
+
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+
+            System.out.println("Music deleted");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Fail to delete this music");
+        }
     }
 
-    public ResultSet listAllMusic(){
+    public void listAllMusic(){
 
         String sql = "SELECT * FROM musics";
         ResultSet musics = null;
@@ -53,11 +79,17 @@ public class MusicsRepository {
         try{
             Statement stmt  = conn.createStatement();
             musics = stmt.executeQuery(sql);
-            return musics;
+            while (musics.next()) {
+                System.out.println(musics.getInt("id") + "\t" + 
+                                   musics.getString("name") + "\t" +
+                                   musics.getString("duration") + "\t" +
+                                   musics.getString("artist") + "\t" +
+                                   musics.getString("category") + "\t");
+            }
 
         }catch(SQLException err){
             System.err.println(err.getMessage());
-            return musics;
+            System.out.println("Nothing to see");
         }
 
     }
